48c9c82c25c5fd5190151e37e9c138daeb9a6e3a
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-07-04 22:51:41 +0800

    Add blur effect

---
+++ Sources/UIKit/User Interface/UIBlurEffect.m
@@ -0,0 +1,50 @@
+/*
+ *  UIBlurEffect.m
+ *  ui-kit
+ *
+ *  Created by Fang Ling on 2026/6/20.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#import "UIBlurEffect.h"
+
+#import "UIBlurEffect+Private.h"
+
+C_ASSUME_NONNULL_BEGIN
+
+@implementation UIBlurEffect
+
++ (instancetype)makeEffectWithStyle:(UIBlurEffectStyle)style {
+  let effect = [[UIBlurEffect alloc] init];
+
+  switch (style) {
+    case kUIBlurEffectStyleSystemChromeMaterial: {
+      effect.background = @"background-material-system-chrome";
+      effect.backgroundBlendMode =
+        @"background-blend-mode-material-system-chrome";
+
+      break;
+    }
+  }
+
+  return effect;
+}
+
+- (instancetype)copy {
+  return self;
+}
+
+@end
+
+C_ASSUME_NONNULL_END
