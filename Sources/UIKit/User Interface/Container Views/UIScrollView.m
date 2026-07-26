f3da75138f37b58d044037b47060bceeffeea155
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-07-26 23:01:59 +0800

    ScrollView: Apply clang-format

--- Sources/UIKit/User Interface/Container Views/UIScrollView.m
+++ Sources/UIKit/User Interface/Container Views/UIScrollView.m
@@ -4,17 +4,14 @@
  *
  *  Created by Fang Ling on 2026/7/11.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ *  See the License for the specific language governing permissions and limitations under the License.
  */
 
 #import "UIScrollView.h"
@@ -37,8 +34,7 @@ C_ASSUME_NONNULL_BEGIN
     return nil;
   }
 
-  self.contentView =
-    [[UIView alloc] initWithFrame:CoreFoundationRectangleMake(0, 0, 0, 0)];
+  self.contentView = [[UIView alloc] initWithFrame:CoreFoundationRectangleMake(0, 0, 0, 0)];
   [super addSubview:self.contentView];
 
   self.contentSize = CoreFoundationSizeMake(0, 0);
@@ -48,12 +44,7 @@ C_ASSUME_NONNULL_BEGIN
 
 - (void)setContentSize:(CoreFoundationSize)contentSize {
   self->_contentSize = contentSize;
-  self.contentView.frame = CoreFoundationRectangleMake(
-    0,
-    0,
-    contentSize.width,
-    contentSize.height
-  );
+  self.contentView.frame = CoreFoundationRectangleMake(0, 0, contentSize.width, contentSize.height);
 
   self.contentView.needsLayout = yes;
 }
